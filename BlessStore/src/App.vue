7cb3bf7a4d5ac75dfe8b41122564c3e5815b246f
750662297 <750662297@qq.com>
2023-06-06 00:58:29 +0800

    change theme

--- BlessStore/src/App.vue
+++ BlessStore/src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Title from "./components/HomeTitle.vue"
 import HomeContent from './components/HomeContent.vue';
 
 </script>
 
 <template>
 	<div class="app">
-		<Title />
+        <!-- <a-layout>
+            <a-layout-header style="background: whitesmoke;height: 40px; padding: 0;padding-bottom: 10px;">
+                
+            </a-layout-header>
+        </a-layout> -->
 		<HomeContent />
 	</div>
 </template>
--- BlessStore/src/components/HomeContent.vue
+++ BlessStore/src/components/HomeContent.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div>
         <a-tabs v-model:activeKey="activeKey">
             <a-tab-pane key="1">
@@ -8,8 +8,7 @@
                         商城
                     </span>
                 </template>
-                <!-- <HomeList /> -->
-                <Store/>
+                <HomeList />
             </a-tab-pane>
             <a-tab-pane key="2" :disabled="isDisabled">
                 <template #tab>
@@ -33,4 +32,74 @@ import Store from './TempStore.vue'
 
 let isDisabled=true;
 
+</script> -->
+
+<template>
+    <a-layout>
+        <a-layout-header class="header" style="height: 50px;margin-bottom: 10px;">
+            <div class="logo" />
+        </a-layout-header>
+
+        <a-layout>
+            <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+                <div class="logo" />
+                <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+                    <a-menu-item key="1">
+                        <shopping-filled />
+                        <span>商城</span>
+                    </a-menu-item>
+                    <a-menu-item key="2" :disabled="isDisabled">
+                        <qq-square-filled />
+                        <span>GM工具</span>
+                    </a-menu-item>
+                </a-menu>
+            </a-layout-sider>
+
+            <a-layout>
+                <a-layout-header style="background: #fff; padding: 0">
+                    <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
+                    <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+                </a-layout-header>
+                <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+                    <HomeList />
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
+
+        <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+</template>
+<script setup>
+import { ShoppingFilled, QqSquareFilled ,MenuUnfoldOutlined, MenuFoldOutlined} from '@ant-design/icons-vue'
+import HomeList from './HomeList.vue'
+
+import { ref } from 'vue';
+
+const selectedKeys = ref(['1']);
+const collapsed = ref(false);
+const isDisabled = ref(true);
 </script>
+
+<style>
+#components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 16px;
+}
+
+.site-layout .site-layout-background {
+    background: #fff;
+}
+</style>
